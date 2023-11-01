64ddae158bdaab10ca736f396b417ef732b2a8bf
HOT <theo.houle@epfl.ch>
2023-11-01 15:20:05 +0100

    Add imdb data in import script

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ In order to run the jupyter notebooks, you will need to download the dataset. To
 ### Manually
 1. Go to http://www.cs.cmu.edu/~ark/personas/ and in "Download" click on the the hyper link "dataset" to download the dataset
 2. Unzip the files into a new folder `./data` at the same level as this README.
+3. Then go to https://datasets.imdbws.com/ and download `title.basics.tsv.gz` and `title.ratings.tsv.gz` 
+4. Decompress the two files in a new folder `./data/imdb/`
 
 
 ### Using the script
@@ -26,12 +28,16 @@ Your data folder should look like this:
 ```
 data/
 └── MovieSummaries/    
-    ├── character.metadata.csv 
-    ├── movie.metadata.csv
+    ├── character.metadata.tsv 
+    ├── movie.metadata.tsv
     ├── name.clusters.txt
     ├── plot_summaries.txt
     ├── README.txt
     └── tvtropes.clusters.txt
+└── imdb/  
+    ├── imdb_titles.tsv
+    └── imdb_ratings.tsv
+
 ```
 
 ## Rendu P2
--- import_data.py
+++ import_data.py
@@ -1,10 +1,15 @@
 import requests, io, gzip, tarfile, os, shutil
 
-data_url = 'http://www.cs.cmu.edu/~ark/personas/data/MovieSummaries.tar.gz'
+movie_url = 'http://www.cs.cmu.edu/~ark/personas/data/MovieSummaries.tar.gz'
+imdb_title_url = 'https://datasets.imdbws.com/title.basics.tsv.gz'
+imdb_rating_url = 'https://datasets.imdbws.com/title.ratings.tsv.gz' 
+
 data_target = './data'
+movie_target = './data/MovieSummaries'
+imdb_target = './data/imdb'
 
 # add function "download_data" that download a dataset from a given URL
-def download_data(url, to):
+def download_movie_data(url, to):
     print("Downloading data from {}".format(url))
     r = requests.get(url, stream=True)
     if r.ok:
@@ -13,10 +18,23 @@ def download_data(url, to):
         tar.extractall(path=to)
         print("Download complete")
 
+def download_imdb_data(url, to):
+    if not os.path.exists(imdb_target):
+        os.mkdir(imdb_target)        
+
+    print("Downloading data from {}".format(url))
+    r = requests.get(url, stream=True)
+    if r.ok:
+        print("Decompressing data to {}".format(to))
+        gzip_obj = gzip.GzipFile(fileobj=io.BytesIO(r.content))
+        with open(to, 'xb') as f:
+            shutil.copyfileobj(gzip_obj, f)
+        print("Download complete")
+
 def check_target(to):
     if os.path.exists(to):
         # ask user if they want to overwrite
-        print("Target directory already exists")
+        print("Data directory already exists")
         response = input("Do you want to overwrite? (y/n): ")
         if response.lower() == 'y':
             print("Overwriting existing directory")
@@ -26,12 +44,17 @@ def check_target(to):
         else:
             print("Exiting")
             return False
+    return True
 
 def main():
     if check_target(data_target):
-        download_data(data_url, data_target)
+        # Download movies data        
+        download_movie_data(movie_url, data_target)
+
+        # Download IMDB data
+        download_imdb_data(imdb_title_url, imdb_target+'/imdb_titles.tsv')
+        download_imdb_data(imdb_rating_url, imdb_target+'/imdb_ratings.tsv')
         
 
 if __name__ == "__main__":
     main()
-        
