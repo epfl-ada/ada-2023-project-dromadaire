bc7962d69e4adf2924fc7f96b0e0f780619e27e7
HOT <theo@houle.pro>
2023-12-22 13:49:51 +0100

    simplify anim

--- docs/assets/js/anim.js
+++ docs/assets/js/anim.js
@@ -1,7 +1,19 @@
+
+function opacityCmp(scrollY, cutOff, maxOpacity=0.95){
+    let clip = scrollY < cutOff ? scrollY : cutOff;
+    let opacity = (1.0/cutOff) * clip;
+    if (opacity > maxOpacity) {
+        return maxOpacity;
+    } else {
+        return opacity;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const element = document.getElementsByClassName("w")[0]
     const title = document.getElementById("logo-div")
     const maxOpacity = 0.95;
+    const cutOff = window.innerHeight*0.75;
     var oldScrollY = window.scrollY;
 
     // Make sure that if we have above 75% of the screen, the opacity is 1
@@ -12,19 +24,14 @@ document.addEventListener("DOMContentLoaded", function() {
     //if(element.style.opacity == "") {
     //    element.style.opacity = "0.0";
     //}
-
-
     
     document.addEventListener("scroll", function(e) {
-        if(window.scrollY < window.innerHeight*0.75) {
-            const opacity = parseFloat(element.style.opacity);
-            var newOpacity = 1/(window.innerHeight*0.75) * window.scrollY;
-            if(newOpacity <= maxOpacity) {
-                element.style.opacity = newOpacity.toString();
-            }
-            title.style.opacity = (1-newOpacity).toString();
+        if(window.scrollY < cutOff) {
+            let opacity = opacityCmp(window.scrollY, cutOff);
+            element.style.opacity = opacity.toString();
+            title.style.opacity = (1-opacity).toString();
         } else {
-            // Make sure the opacity is 1
+            // Make sure the opacity is max
             element.style.opacity = maxOpacity.toString();
         }
 
