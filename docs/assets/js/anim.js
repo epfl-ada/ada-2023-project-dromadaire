4ed2faf4742323af90d4faf6fc7ea8cf05c4efe3
HOT <theo@houle.pro>
2023-12-22 13:25:51 +0100

    Change list colours

--- docs/assets/js/anim.js
+++ docs/assets/js/anim.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const element = document.getElementsByClassName("w")[0]
-
+    const title = document.getElementById("logo-div")
+    const maxOpacity = 0.95;
     var oldScrollY = window.scrollY;
 
     // Make sure that if we have above 75% of the screen, the opacity is 1
     if(window.scrollY > window.innerHeight*0.75) {
-       element.style.opacity = "1.0";
+       element.style.opacity = maxOpacity.toString();
     }
 
     //if(element.style.opacity == "") {
@@ -18,18 +19,19 @@ document.addEventListener("DOMContentLoaded", function() {
         if(window.scrollY < window.innerHeight*0.75) {
             const opacity = parseFloat(element.style.opacity);
             var newOpacity = 1/(window.innerHeight*0.75) * window.scrollY;
-            element.style.opacity = newOpacity.toString();
+            if(newOpacity <= maxOpacity) {
+                element.style.opacity = newOpacity.toString();
+            }
+            title.style.opacity = (1-newOpacity).toString();
         } else {
             // Make sure the opacity is 1
-            element.style.opacity = "1.0";
+            element.style.opacity = maxOpacity.toString();
         }
 
         oldScrollY = window.scrollY;
     });
-});
 
-viridis.forEach((rgb, index) => {
-    const cssVariableName = `--viridis-${index}`;
-    const cssVariableValue = `rgb(${rgb.join(', ')})`;
-    styleTag.innerHTML += ` ${cssVariableName}: ${cssVariableValue};`;
+    window.addEventListener("popstate", function(e) {
+        console.log(e);   
+    });
 });
